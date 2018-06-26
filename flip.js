59f14702b023e2b3dcb850ef0b5804dd40bb3451
John Goodman <johncurtisgoodman@gmail.com>
2018-06-26 11:58:52 -0400

    Draw images to canvas as filmstrip for download.

--- flip.js
+++ flip.js
@@ -1,12 +1,16 @@
-window.receiveFiles = () => {
+let receiveFiles = () => {
     let preview = document.getElementById('preview-section')
+    let convertButton = document.getElementById('multi-image-convert')
     let fileSource = document.querySelector('input[type=file]')
+    let images = []
 
     Array.from(fileSource.files).map(file => {
+        let previewImage = document.createElement('img')
         let output = document.createElement('img')
         let reader  = new FileReader()
         
         reader.addEventListener('load', () => {
+            previewImage.src = reader.result
             output.src = reader.result
         }, false)
         
@@ -14,8 +18,30 @@ window.receiveFiles = () => {
             reader.readAsDataURL(file)
         }
         
-        preview.appendChild(output)
+        preview.appendChild(previewImage)
+        images.push(output)
     })
+    
+    convertButton.addEventListener('click', () => {
+        makeFilmStrip(images)
+    })
+}
+
+let sum = (total, num) => {
+    return total + num;
+}
+
+let makeFilmStrip = images => {
+    let canvas = document.getElementById('output-canvas')
+    let ctx = canvas.getContext('2d')
+    window.images = images
+    canvas.height = images[0].height
+    canvas.width = images.map(img => { return img.width }).reduce(sum)
+    images.map((image, index) => {
+        let x = images.map((img, ind) => { return ind >= index ? 0 : img.width }).reduce(sum)
+        ctx.drawImage(image, x, 0)
+    })
+    document.getElementById('download-info').innerText = 'Right click canvas (above) to download filmstrip as image.'
 }
 
 window.addEventListener ('load', () => {
